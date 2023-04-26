48e7155668e5ea9defa734318cfd08694b44d6f3
Leo1014 <las1014@gmail.com>
2023-04-26 13:50:29 -0500

    Initial

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,34 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Recipe', {
+    id:{
+      type:DataTypes.UUID,
+      primaryKey:true,
+      defaultValue:UUIDV4
+    },    
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    summary:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    },
+    healthScore:{
+      type:DataTypes.INTEGER,
+      allowNull:false
+    },
+    stepByStep:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    }
+
+  },{timestamps:false})
+};
