b451c7d228c1ddaddb182ef3a8ec88b81d393ce9
Leo1014 <las1014@gmail.com>
2023-04-26 16:40:22 -0500

    advance

--- api/src/controllers/DietsControllers.js
+++ api/src/controllers/DietsControllers.js
@@ -8,16 +8,18 @@ const getDiets = async ()=>{
     const data = (await axios.get
         ("https://api.spoonacular.com/recipes/complexSearch?apiKey=9bd9403db46a4af6bfaa8961373554b5&addRecipeInformation=true&number=100")).data
     const results = data.results.map(result=>result.diets).flat();
-    const diets = results.map(diet=>{
-        return{
-            name:diet
-        }
-    })
+    const resultados = results.map(result => {
+        return Object.keys(result)
+      });
+
+    //console.log(resultados[0][0]);
+    
     const uniqueDiets = new Set(results);
     const dietsUnique = [...uniqueDiets].map((diet) => ({ name: diet }));
 
-    //const saveDb = await Diet.bulkCreate(diets)
-    console.log(dietsUnique);
+    const saveDb = await Diet.bulkCreate(dietsUnique)
+    console.log(saveDb);
+    return saveDb;
 }
 
-getDiets().then(results => console.log(results));
+//getDiets().then(results => console.log(results));
